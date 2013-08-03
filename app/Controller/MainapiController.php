454b3a73ec08c636de13f5754be6dac8ed3c09c3
sybil <xichen0603@gmail.com>
2013-08-04 00:46:13 +0800

    MOD: album info api

--- app/Controller/MainapiController.php
+++ app/Controller/MainapiController.php
@@ -19,7 +19,7 @@ class MainapiController extends AppController {
 		$albums = $albums_col->find();
 		$info = array();
 		foreach ($albums as $album) {
-			$info[] = $album;
+			$info[] = $this->copyAlbum($album);
 		}
 		echo json_encode($info);
 	}
@@ -29,6 +29,28 @@ class MainapiController extends AppController {
 		$album_id = $this->_get_argument('id');
 		$albums = $this->get_collection($this->db_name, $this->album_collection);
 		$album = $albums->findOne(array('_id' => $album_id));
-		echo json_encode($album);
+		$info = $this->copyAlbum($album);
+		echo json_encode($info);
+	}
+
+	private function copyAlbum($album)
+	{
+		$al = array();
+		$al['id'] = $album['_id'];
+		$al['desc'] = $album['desc'];
+		$al['title'] = $album['title'];
+		if (isset($album['cover'])) {
+			$cover_id = $album['cover'];
+			$al['cover']['large'] = $album['images'][$cover_id]['large'];
+			$al['cover']['small'] = $album['images'][$cover_id]['small'];
+		}
+		$images = array();
+		if (isset($album['images'])) {
+			foreach ($album['images'] as $image) {
+				$images[] = $image;
+			}
+		}
+		$al['images'] = $images;
+		return $al;
 	}
 }
