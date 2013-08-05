a38f49b47aa8d1680e4a5305acb544af2823639b
huya.nzb <huya.nzb@taobao.com>
2013-08-05 21:30:32 +0800

    teachers

---
+++ app/webroot/teacher/js/config-min.js
@@ -0,0 +1,2 @@
+/* ZD */
+!function(){ZD.applyConfig({groups:{index:{base:YUI.getBase("teacher/js"),comboBase:YUI.getComboBase("teacher/js"),root:"",combine:YUI.combine,modules:{main:{use:[]}}}}})}();
