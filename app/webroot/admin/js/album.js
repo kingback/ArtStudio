db83143fa1b430fb5ea8d8fc4c6bbf93f9dfbfd7
sybil <xichen0603@gmail.com>
2013-08-17 18:27:04 +0800

    FIX: js

--- app/webroot/admin/js/album.js
+++ app/webroot/admin/js/album.js
@@ -206,7 +206,7 @@ function deleteAlbum() {
 		},
 		success: function(e) {
 			alert("删除成功");
-			//window.location.reload();
+			window.location.reload();
 		},
 		error: function(XMLHttpRequest, textStatus, errorThrown) {
 			alert("删除失败");
